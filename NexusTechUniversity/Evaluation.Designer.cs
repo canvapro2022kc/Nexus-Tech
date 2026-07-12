bf5de2b3b7db9abcb635a0c836049ddcd506db61
llantoalyssa <llantoalyssa@gmail.com>
2026-07-12 11:25:08 +0800

    Add Student Form - Added Print POS, removed "Courses Taken", Labeled "Courses" for left dgv.

--- NexusTechUniversity/Evaluation.Designer.cs
+++ NexusTechUniversity/Evaluation.Designer.cs
@@ -33,11 +33,8 @@
             pictureBox1 = new PictureBox();
             label7 = new Label();
             label9 = new Label();
-            comboBox3 = new ComboBox();
-            comboBox1 = new ComboBox();
             comboBox2 = new ComboBox();
             label4 = new Label();
-            textBox3 = new TextBox();
             dgvNewStudent = new DataGridView();
             label6 = new Label();
             label5 = new Label();
@@ -54,6 +51,11 @@
             label10 = new Label();
             label11 = new Label();
             button4 = new Button();
+            label12 = new Label();
+            button1 = new Button();
+            textBox3 = new TextBox();
+            comboBox1 = new ComboBox();
+            comboBox3 = new ComboBox();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dgvNewStudent).BeginInit();
@@ -63,18 +65,20 @@
             // 
             // panel1
             // 
+            panel1.Controls.Add(comboBox3);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(textBox3);
             panel1.Controls.Add(button4);
+            panel1.Controls.Add(btnAddStudent);
             panel1.Controls.Add(label11);
+            panel1.Controls.Add(label8);
             panel1.Controls.Add(comboBox4);
             panel1.Controls.Add(label10);
             panel1.Controls.Add(pictureBox1);
             panel1.Controls.Add(label7);
             panel1.Controls.Add(label9);
-            panel1.Controls.Add(comboBox3);
-            panel1.Controls.Add(comboBox1);
             panel1.Controls.Add(comboBox2);
             panel1.Controls.Add(label4);
-            panel1.Controls.Add(textBox3);
             panel1.Controls.Add(dgvNewStudent);
             panel1.Controls.Add(label6);
             panel1.Controls.Add(label5);
@@ -122,67 +126,40 @@
             label9.TabIndex = 23;
             label9.Text = "NexusTech";
             // 
-            // comboBox3
-            // 
-            comboBox3.FormattingEnabled = true;
-            comboBox3.Location = new Point(191, 115);
-            comboBox3.Margin = new Padding(3, 2, 3, 2);
-            comboBox3.Name = "comboBox3";
-            comboBox3.Size = new Size(134, 23);
-            comboBox3.TabIndex = 21;
-            comboBox3.SelectedIndexChanged += comboBox3_SelectedIndexChanged;
-            // 
-            // comboBox1
-            // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(352, 115);
-            comboBox1.Margin = new Padding(3, 2, 3, 2);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(134, 23);
-            comboBox1.TabIndex = 20;
-            // 
             // comboBox2
             // 
             comboBox2.FormattingEnabled = true;
             comboBox2.Location = new Point(26, 115);
             comboBox2.Margin = new Padding(3, 2, 3, 2);
             comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(134, 23);
+            comboBox2.Size = new Size(159, 23);
             comboBox2.TabIndex = 19;
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Tw Cen MT", 9.75F);
-            label4.Location = new Point(352, 98);
+            label4.Location = new Point(367, 99);
             label4.Name = "label4";
             label4.Size = new Size(90, 15);
             label4.TabIndex = 15;
             label4.Text = "Academic Year:";
             // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(292, 74);
-            textBox3.Margin = new Padding(3, 2, 3, 2);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(194, 23);
-            textBox3.TabIndex = 14;
-            // 
             // dgvNewStudent
             // 
             dgvNewStudent.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvNewStudent.Location = new Point(24, 209);
+            dgvNewStudent.Location = new Point(26, 241);
             dgvNewStudent.Margin = new Padding(3, 2, 3, 2);
             dgvNewStudent.Name = "dgvNewStudent";
             dgvNewStudent.RowHeadersWidth = 51;
-            dgvNewStudent.Size = new Size(497, 203);
+            dgvNewStudent.Size = new Size(497, 169);
             dgvNewStudent.TabIndex = 12;
             // 
             // label6
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Tw Cen MT", 9.75F);
-            label6.Location = new Point(191, 98);
+            label6.Location = new Point(195, 98);
             label6.Name = "label6";
             label6.Size = new Size(58, 15);
             label6.TabIndex = 11;
@@ -200,7 +177,7 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(236, 74);
+            textBox2.Location = new Point(255, 74);
             textBox2.Margin = new Padding(3, 2, 3, 2);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(42, 23);
@@ -210,7 +187,7 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Tw Cen MT", 9.75F);
-            label3.Location = new Point(236, 57);
+            label3.Location = new Point(255, 57);
             label3.Name = "label3";
             label3.Size = new Size(26, 15);
             label3.TabIndex = 3;
@@ -220,7 +197,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Tw Cen MT", 9.75F);
-            label2.Location = new Point(292, 57);
+            label2.Location = new Point(310, 57);
             label2.Name = "label2";
             label2.Size = new Size(65, 15);
             label2.TabIndex = 2;
@@ -241,14 +218,14 @@
             textBox1.Location = new Point(26, 74);
             textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(194, 23);
+            textBox1.Size = new Size(213, 23);
             textBox1.TabIndex = 0;
             // 
             // panel2
             // 
-            panel2.Controls.Add(btnAddStudent);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(label12);
             panel2.Controls.Add(dgvCoursesTaken);
-            panel2.Controls.Add(label8);
             panel2.Location = new Point(571, 9);
             panel2.Margin = new Padding(3, 2, 3, 2);
             panel2.Name = "panel2";
@@ -262,7 +239,7 @@
             btnAddStudent.FlatStyle = FlatStyle.Flat;
             btnAddStudent.Font = new Font("Tw Cen MT", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnAddStudent.ForeColor = Color.White;
-            btnAddStudent.Location = new Point(151, 421);
+            btnAddStudent.Location = new Point(412, 417);
             btnAddStudent.Margin = new Padding(3, 2, 3, 2);
             btnAddStudent.Name = "btnAddStudent";
             btnAddStudent.Size = new Size(111, 23);
@@ -277,19 +254,19 @@
             dgvCoursesTaken.Margin = new Padding(3, 2, 3, 2);
             dgvCoursesTaken.Name = "dgvCoursesTaken";
             dgvCoursesTaken.RowHeadersWidth = 51;
-            dgvCoursesTaken.Size = new Size(386, 372);
+            dgvCoursesTaken.Size = new Size(386, 373);
             dgvCoursesTaken.TabIndex = 12;
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Font = new Font("Tw Cen MT", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Font = new Font("Tw Cen MT", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label8.ForeColor = Color.Navy;
-            label8.Location = new Point(3, 2);
+            label8.Location = new Point(26, 214);
             label8.Name = "label8";
-            label8.Size = new Size(148, 25);
+            label8.Size = new Size(85, 24);
             label8.TabIndex = 11;
-            label8.Text = "Courses Taken";
+            label8.Text = "Courses ";
             // 
             // comboBox4
             // 
@@ -297,7 +274,7 @@
             comboBox4.Location = new Point(26, 157);
             comboBox4.Margin = new Padding(3, 2, 3, 2);
             comboBox4.Name = "comboBox4";
-            comboBox4.Size = new Size(134, 23);
+            comboBox4.Size = new Size(497, 23);
             comboBox4.TabIndex = 27;
             // 
             // label10
@@ -315,7 +292,7 @@
             label11.AutoSize = true;
             label11.Font = new Font("Tw Cen MT", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label11.ForeColor = Color.Red;
-            label11.Location = new Point(28, 180);
+            label11.Location = new Point(27, 180);
             label11.Name = "label11";
             label11.Size = new Size(107, 14);
             label11.TabIndex = 28;
@@ -338,6 +315,58 @@
             button4.Size = new Size(18, 22);
             button4.TabIndex = 57;
             button4.UseVisualStyleBackColor = false;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Tw Cen MT", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.ForeColor = Color.Navy;
+            label12.Location = new Point(11, 10);
+            label12.Name = "label12";
+            label12.Size = new Size(174, 25);
+            label12.TabIndex = 58;
+            label12.Text = "Program of Study";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Navy;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Tw Cen MT", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(11, 417);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(386, 23);
+            button1.TabIndex = 58;
+            button1.Text = "Print Program of Study";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(310, 74);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(213, 23);
+            textBox3.TabIndex = 58;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(195, 115);
+            comboBox1.Margin = new Padding(3, 2, 3, 2);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(159, 23);
+            comboBox1.TabIndex = 59;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(364, 116);
+            comboBox3.Margin = new Padding(3, 2, 3, 2);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(159, 23);
+            comboBox3.TabIndex = 60;
             // 
             // Evaluation
             // 
@@ -371,11 +400,8 @@
         private Label label5;
         private Label label6;
         private DataGridView dgvNewStudent;
-        private ComboBox comboBox3;
-        private ComboBox comboBox1;
         private ComboBox comboBox2;
         private Label label4;
-        private TextBox textBox3;
         private Panel panel2;
         private Label label8;
         private DataGridView dgvCoursesTaken;
@@ -387,5 +413,10 @@
         private ComboBox comboBox4;
         private Label label10;
         private Button button4;
+        private Button button1;
+        private Label label12;
+        private ComboBox comboBox3;
+        private ComboBox comboBox1;
+        private TextBox textBox3;
     }
 }
