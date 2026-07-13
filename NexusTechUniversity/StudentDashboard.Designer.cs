ddf79bcc2482953337b97d383d78b8d60d62317e
Ella-Claveria <ellaclaveria14@gmail.com>
2026-07-13 14:12:02 +0800

    students login

--- NexusTechUniversity/StudentDashboard.Designer.cs
+++ NexusTechUniversity/StudentDashboard.Designer.cs
@@ -30,34 +30,34 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StudentDashboard));
             panel1 = new Panel();
-            pictureBox1 = new PictureBox();
-            label2 = new Label();
-            label1 = new Label();
-            btnAddStudent = new Button();
-            panel2 = new Panel();
-            label4 = new Label();
-            label3 = new Label();
-            label5 = new Label();
-            label6 = new Label();
-            dataGridView1 = new DataGridView();
+            btnExportPOS = new Button();
+            dgvCourses = new DataGridView();
             courseCode = new DataGridViewTextBoxColumn();
             courseName = new DataGridViewTextBoxColumn();
             courseDescription = new DataGridViewTextBoxColumn();
             units = new DataGridViewTextBoxColumn();
             remarks = new DataGridViewTextBoxColumn();
-            button1 = new Button();
+            panel2 = new Panel();
+            lblAcadYear = new Label();
+            lblDepartment = new Label();
+            lblStatus = new Label();
+            lblfName = new Label();
+            btnLogout = new Button();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label1 = new Label();
             panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvCourses).BeginInit();
             panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
-            panel1.Controls.Add(button1);
-            panel1.Controls.Add(dataGridView1);
+            panel1.Controls.Add(btnExportPOS);
+            panel1.Controls.Add(dgvCourses);
             panel1.Controls.Add(panel2);
-            panel1.Controls.Add(btnAddStudent);
+            panel1.Controls.Add(btnLogout);
             panel1.Controls.Add(pictureBox1);
             panel1.Controls.Add(label2);
             panel1.Controls.Add(label1);
@@ -67,119 +67,32 @@
             panel1.Size = new Size(1133, 619);
             panel1.TabIndex = 0;
             // 
-            // pictureBox1
+            // btnExportPOS
             // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(52, 48);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(47, 45);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 7;
-            pictureBox1.TabStop = false;
+            btnExportPOS.BackColor = Color.Navy;
+            btnExportPOS.FlatAppearance.BorderSize = 0;
+            btnExportPOS.FlatStyle = FlatStyle.Flat;
+            btnExportPOS.Font = new Font("Tw Cen MT", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnExportPOS.ForeColor = Color.White;
+            btnExportPOS.Location = new Point(951, 558);
+            btnExportPOS.Name = "btnExportPOS";
+            btnExportPOS.Size = new Size(127, 31);
+            btnExportPOS.TabIndex = 11;
+            btnExportPOS.Text = "Export POS";
+            btnExportPOS.UseVisualStyleBackColor = false;
+            btnExportPOS.Click += btnExportPOS_Click;
             // 
-            // label2
+            // dgvCourses
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Tw Cen MT Condensed Extra Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.ForeColor = Color.Navy;
-            label2.Location = new Point(109, 73);
-            label2.Name = "label2";
-            label2.Size = new Size(74, 21);
-            label2.TabIndex = 6;
-            label2.Text = "University";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Tw Cen MT Condensed Extra Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.ForeColor = Color.Navy;
-            label1.Location = new Point(109, 52);
-            label1.Name = "label1";
-            label1.Size = new Size(90, 23);
-            label1.TabIndex = 5;
-            label1.Text = "NexusTech";
-            // 
-            // btnAddStudent
-            // 
-            btnAddStudent.BackColor = Color.White;
-            btnAddStudent.FlatAppearance.BorderSize = 0;
-            btnAddStudent.FlatStyle = FlatStyle.Flat;
-            btnAddStudent.Font = new Font("Tw Cen MT", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnAddStudent.ForeColor = Color.Navy;
-            btnAddStudent.Location = new Point(951, 48);
-            btnAddStudent.Name = "btnAddStudent";
-            btnAddStudent.Size = new Size(127, 31);
-            btnAddStudent.TabIndex = 8;
-            btnAddStudent.Text = "LOG OUT";
-            btnAddStudent.UseVisualStyleBackColor = false;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.White;
-            panel2.Controls.Add(label6);
-            panel2.Controls.Add(label5);
-            panel2.Controls.Add(label3);
-            panel2.Controls.Add(label4);
-            panel2.Location = new Point(52, 138);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(387, 166);
-            panel2.TabIndex = 9;
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Tw Cen MT Condensed Extra Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.ForeColor = Color.Navy;
-            label4.Location = new Point(23, 30);
-            label4.Name = "label4";
-            label4.Size = new Size(143, 23);
-            label4.TabIndex = 11;
-            label4.Text = "FName M. LName";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Tw Cen MT", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.ForeColor = Color.Navy;
-            label3.Location = new Point(23, 96);
-            label3.Name = "label3";
-            label3.Size = new Size(113, 17);
-            label3.TabIndex = 12;
-            label3.Text = "• A.Y. 2025-2026";
-            label3.Click += label3_Click;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Tw Cen MT", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.ForeColor = Color.Navy;
-            label5.Location = new Point(23, 121);
-            label5.Name = "label5";
-            label5.Size = new Size(91, 20);
-            label5.TabIndex = 13;
-            label5.Text = "• ENROLLED";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Tw Cen MT", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label6.ForeColor = Color.Navy;
-            label6.Location = new Point(23, 68);
-            label6.Name = "label6";
-            label6.Size = new Size(331, 20);
-            label6.TabIndex = 14;
-            label6.Text = "• College of Infomatics and Computing Sciences";
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { courseCode, courseName, courseDescription, units, remarks });
-            dataGridView1.Location = new Point(481, 138);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersVisible = false;
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(597, 401);
-            dataGridView1.TabIndex = 10;
+            dgvCourses.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCourses.Columns.AddRange(new DataGridViewColumn[] { courseCode, courseName, courseDescription, units, remarks });
+            dgvCourses.Location = new Point(481, 138);
+            dgvCourses.Name = "dgvCourses";
+            dgvCourses.RowHeadersVisible = false;
+            dgvCourses.RowHeadersWidth = 51;
+            dgvCourses.Size = new Size(597, 401);
+            dgvCourses.TabIndex = 10;
+            dgvCourses.CellContentClick += dgvCourses_CellContentClick;
             // 
             // courseCode
             // 
@@ -216,19 +129,111 @@
             remarks.Name = "remarks";
             remarks.Width = 125;
             // 
-            // button1
+            // panel2
             // 
-            button1.BackColor = Color.Navy;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Tw Cen MT", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.ForeColor = Color.White;
-            button1.Location = new Point(951, 558);
-            button1.Name = "button1";
-            button1.Size = new Size(127, 31);
-            button1.TabIndex = 11;
-            button1.Text = "Export POS";
-            button1.UseVisualStyleBackColor = false;
+            panel2.BackColor = Color.White;
+            panel2.Controls.Add(lblAcadYear);
+            panel2.Controls.Add(lblDepartment);
+            panel2.Controls.Add(lblStatus);
+            panel2.Controls.Add(lblfName);
+            panel2.Location = new Point(52, 138);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(387, 166);
+            panel2.TabIndex = 9;
+            // 
+            // lblAcadYear
+            // 
+            lblAcadYear.AutoSize = true;
+            lblAcadYear.Font = new Font("Tw Cen MT", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblAcadYear.ForeColor = Color.Navy;
+            lblAcadYear.Location = new Point(23, 92);
+            lblAcadYear.Name = "lblAcadYear";
+            lblAcadYear.Size = new Size(113, 17);
+            lblAcadYear.TabIndex = 15;
+            lblAcadYear.Text = "• A.Y. 2025-2026";
+            lblAcadYear.Click += lblAcadyear_Click;
+            // 
+            // lblDepartment
+            // 
+            lblDepartment.AutoSize = true;
+            lblDepartment.Font = new Font("Tw Cen MT", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblDepartment.ForeColor = Color.Navy;
+            lblDepartment.Location = new Point(23, 61);
+            lblDepartment.Name = "lblDepartment";
+            lblDepartment.Size = new Size(331, 20);
+            lblDepartment.TabIndex = 14;
+            lblDepartment.Text = "• College of Infomatics and Computing Sciences";
+            lblDepartment.Click += lblDepartment_Click;
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Font = new Font("Tw Cen MT", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblStatus.ForeColor = Color.Navy;
+            lblStatus.Location = new Point(22, 119);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(91, 20);
+            lblStatus.TabIndex = 13;
+            lblStatus.Text = "• ENROLLED";
+            // 
+            // lblfName
+            // 
+            lblfName.AutoSize = true;
+            lblfName.Font = new Font("Tw Cen MT Condensed Extra Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblfName.ForeColor = Color.Navy;
+            lblfName.Location = new Point(23, 28);
+            lblfName.Name = "lblfName";
+            lblfName.Size = new Size(143, 23);
+            lblfName.TabIndex = 11;
+            lblfName.Text = "FName M. LName";
+            lblfName.Click += lblfName_Click;
+            // 
+            // btnLogout
+            // 
+            btnLogout.BackColor = Color.White;
+            btnLogout.FlatAppearance.BorderSize = 0;
+            btnLogout.FlatStyle = FlatStyle.Flat;
+            btnLogout.Font = new Font("Tw Cen MT", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLogout.ForeColor = Color.Navy;
+            btnLogout.Location = new Point(951, 48);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(127, 31);
+            btnLogout.TabIndex = 8;
+            btnLogout.Text = "LOG OUT";
+            btnLogout.UseVisualStyleBackColor = false;
+            btnLogout.Click += btnLogout_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(52, 48);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(47, 45);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Tw Cen MT Condensed Extra Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Navy;
+            label2.Location = new Point(109, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 21);
+            label2.TabIndex = 6;
+            label2.Text = "University";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tw Cen MT Condensed Extra Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Navy;
+            label1.Location = new Point(109, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 23);
+            label1.TabIndex = 5;
+            label1.Text = "NexusTech";
             // 
             // StudentDashboard
             // 
@@ -238,12 +243,13 @@
             Controls.Add(panel1);
             Name = "StudentDashboard";
             Text = "StudentDashboard";
+            Load += StudentDashboard_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvCourses).EndInit();
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -254,17 +260,17 @@
         private Label label2;
         private Label label1;
         private Panel panel2;
-        private Label label3;
-        private Label label4;
-        private Button btnAddStudent;
-        private Label label6;
-        private Label label5;
-        private DataGridView dataGridView1;
+        private Label lblfName;
+        private Button btnLogout;
+        private Label lblDepartment;
+        private Label lblStatus;
+        private DataGridView dgvCourses;
         private DataGridViewTextBoxColumn courseCode;
         private DataGridViewTextBoxColumn courseName;
         private DataGridViewTextBoxColumn courseDescription;
         private DataGridViewTextBoxColumn units;
         private DataGridViewTextBoxColumn remarks;
-        private Button button1;
+        private Button btnExportPOS;
+        private Label lblAcadYear;
     }
 }
--- NexusTechUniversity/StudentDashboard.cs
+++ NexusTechUniversity/StudentDashboard.cs
@@ -1,21 +1,261 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using Google.Cloud.Firestore;
+using System;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace NexusTechUniversity
 {
     public partial class StudentDashboard : Form
     {
-        public StudentDashboard()
+        // ==== Firestore connection (created once, shared) ====
+        private static FirestoreDb? _db;
+        private static FirestoreDb Db
         {
-            InitializeComponent();
+            get
+            {
+                if (_db == null)
+                {
+                    string projectId = "enrollmentit331";
+
+                    // serviceAccountKey.json must sit next to the .exe (bin\Debug).
+                    // Set its "Copy to Output Directory" to "Copy if newer".
+                    string credPath = Path.Combine(Application.StartupPath, "serviceAccountKey.json");
+                    Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credPath);
+
+                    _db = FirestoreDb.Create(projectId);
+                }
+                return _db;
+            }
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private readonly string userRole;
+        private readonly string? studentId;
+
+        // Only one constructor. If anything calls this with a missing argument,
+        // the project won't compile — which prevents the "empty studentId" bug.
+        public StudentDashboard(string role, string? studentId)
+        {
+            InitializeComponent();
+
+            this.userRole = role;
+            this.studentId = studentId;
+
+            if (userRole == "student")
+            {
+                ApplyViewOnlyMode();
+            }
+        }
+
+        private void ApplyViewOnlyMode()
+        {
+            this.Text = "Nexus Tech University - Student Dashboard (View Only)";
+        }
+
+        // ==== Form load ====
+        private async void StudentDashboard_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                await LoadStudentInfo();
+                await LoadCourses();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load dashboard data:\n" + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // ==== Student panel (name, college, A.Y., status) ====
+        private async Task LoadStudentInfo()
+        {
+            if (string.IsNullOrEmpty(studentId))
+            {
+                MessageBox.Show("No student is currently logged in.",
+                    "Missing student", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DocumentSnapshot doc = await Db.Collection("users")
+                                           .Document(studentId)
+                                           .GetSnapshotAsync();
+
+            if (!doc.Exists)
+            {
+                MessageBox.Show($"Student record '{studentId}' not found.",
+                    "Not found", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Get the student's information from Firestore
+            string first = GetField(doc, "firstName");
+            string middle = GetField(doc, "middleName");   // Optional
+            string last = GetField(doc, "lastName");
+            string college = GetField(doc, "college");
+            string acadYear = GetField(doc, "academicYear");
+            string status = GetField(doc, "status");
+
+            // Build the middle initial
+            string middleInitial = string.IsNullOrEmpty(middle)
+                ? ""
+                : middle.Substring(0, 1).ToUpper() + ". ";
+
+            // Display the data on the labels
+            lblfName.Text = $"{first} {middleInitial}{last}";
+            lblDepartment.Text = "• " + college;
+            lblAcadYear.Text = "• A.Y. " + acadYear;
+            lblStatus.Text = "• " + status.ToUpper();
+        }
+
+        // ==== Course grid ====
+        private async Task LoadCourses()
+        {
+            if (string.IsNullOrEmpty(studentId)) return;
+
+            dgvCourses.Rows.Clear();
+
+            QuerySnapshot snap = await Db.Collection("users")
+                                         .Document(studentId)
+                                         .Collection("courses")
+                                         .GetSnapshotAsync();
+
+            foreach (DocumentSnapshot d in snap.Documents)
+            {
+                dgvCourses.Rows.Add(
+                    GetField(d, "CourseCode"),
+                    GetField(d, "CourseTitle"),
+                    GetField(d, "CourseDescription"),
+                    GetField(d, "Units"),
+                    (d.TryGetValue("Passed", out bool p) && p) ? "PASSED" : "ENROLLED"
+                );
+            }
+
+            if (dgvCourses.Rows.Count == 0)
+            {
+                MessageBox.Show("No enrolled courses found for this student.",
+                    "No courses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // Safe reader – never returns null
+        private static string GetField(DocumentSnapshot doc, string field)
+        {
+            if (doc.TryGetValue(field, out object value) && value != null)
+                return value.ToString() ?? "";
+            return "";
+        }
+
+        // ==== Logout ====
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            DialogResult confirm = MessageBox.Show(
+                "Are you sure you want to log out?",
+                "Log Out", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (confirm == DialogResult.Yes)
+            {
+                var login = new Form1();
+                login.Show();
+                this.Close();
+            }
+        }
+
+        // ==== Export POS (Program of Study) ====
+        private void btnExportPOS_Click(object sender, EventArgs e)
+        {
+            if (dgvCourses.Rows.Count == 0)
+            {
+                MessageBox.Show("There are no courses to export.",
+                    "Nothing to export", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            using (var sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "Text file (*.txt)|*.txt|CSV file (*.csv)|*.csv";
+                sfd.FileName = $"POS_{(string.IsNullOrEmpty(studentId) ? "student" : studentId)}_{DateTime.Now:yyyyMMdd}.txt";
+
+                if (sfd.ShowDialog() != DialogResult.OK) return;
+
+                try
+                {
+                    var sb = new StringBuilder();
+                    bool csv = sfd.FilterIndex == 2;
+
+                    sb.AppendLine("NEXUS TECH UNIVERSITY");
+                    sb.AppendLine("PROGRAM OF STUDY (POS)");
+                    sb.AppendLine(new string('-', 60));
+                    sb.AppendLine($"Student : {lblfName.Text}");
+                    sb.AppendLine($"College : {lblDepartment.Text.TrimStart('•', ' ')}");
+                    sb.AppendLine($"{lblAcadYear.Text.TrimStart('•', ' ')}");
+                    sb.AppendLine($"Status  : {lblStatus.Text.TrimStart('•', ' ')}");
+                    sb.AppendLine(new string('-', 60));
+                    sb.AppendLine();
+
+                    if (csv)
+                        sb.AppendLine("Course Code,Course Name,Course Description,Units,Remarks");
+
+                    foreach (DataGridViewRow row in dgvCourses.Rows)
+                    {
+                        if (row.IsNewRow) continue;
+
+                        string code = Cell(row, 0);
+                        string name = Cell(row, 1);
+                        string desc = Cell(row, 2);
+                        string units = Cell(row, 3);
+                        string remarks = Cell(row, 4);
+
+                        if (csv)
+                            sb.AppendLine($"{Q(code)},{Q(name)},{Q(desc)},{Q(units)},{Q(remarks)}");
+                        else
+                            sb.AppendLine($"{code,-12}{name,-25}{units,-8}{remarks}");
+                    }
+
+                    sb.AppendLine();
+                    sb.AppendLine($"Generated: {DateTime.Now:g}");
+
+                    File.WriteAllText(sfd.FileName, sb.ToString());
+
+                    MessageBox.Show("Program of Study exported successfully.",
+                        "Export complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Failed to export POS:\n" + ex.Message,
+                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private static string Cell(DataGridViewRow row, int index)
+            => row.Cells[index].Value?.ToString() ?? "";
+
+        private static string Q(string s)
+            => s.Contains(",") || s.Contains("\"")
+               ? "\"" + s.Replace("\"", "\"\"") + "\""
+               : s;
+
+        // ---- Designer click handlers (intentionally empty; view-only form) ----
+        private void lblAcadyear_Click(object sender, EventArgs e) 
+        { 
+        
+        }
+        private void lblfName_Click(object sender, EventArgs e) 
+        { 
+        
+        }
+        private void lblDepartment_Click(object sender, EventArgs e) 
+        { 
+        
+        }
+        private void dgvCourses_CellContentClick(object sender, DataGridViewCellEventArgs e) 
+        {
+        
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
         {
 
         }
